9889904d7f07ed32e1c721960326b255dde85024
verocoder <david@verophotography.co.uk>
2017-04-29 16:31:06 +0100

    added json decode

--- davidDemo.py
+++ davidDemo.py
@@ -1,8 +1,9 @@
 import requests
 
 #This gets a noddy json document and prints it out
+#this apparently is a python 2.7 example
 
 r = requests.get("http://jsonplaceholder.typicode.com/posts/1")
-print r.status_code
-print r.headers
-print r.content
+json = r.json()
+print (r.status_code)
+print (json)
